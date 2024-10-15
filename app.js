d1719a46cb05731e5dba31d00a74706415aa1052
Hari <harikrishkk@gmail.com>
2024-10-15 14:22:07 -0400

    add security

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
+
+// extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
 const app = express();
 
 // connectDB
@@ -15,7 +22,15 @@ const authRouter = require('./routes/auth');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.use(rateLimiter({
+  windowMs: 15 * 60 * 1000, // `15 minutes`
+  max: 100 // limit each IP to 100 requests per `window` (here, per 15 minutes)
+}));
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
 // extra packages
 
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
